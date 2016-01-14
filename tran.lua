1b84308516070b2edf079aec60ed81bd5cc24bb5
smltiger <28286880@qq.com>
2016-01-14 23:04:34 +0800

    a

--- tran.lua
+++ tran.lua
@@ -58,18 +58,14 @@ function loadCsvFile(filePath)
     end  
     --[[ 从第2行开始保存（第一行是标题，后面的行才是内容） 用二维数组保存：arr[ID][属性标题字符串] ]]  
     local titles = split(alls[1], ",")  
-    local ID = 1  
     local arrs = {}  
     for i = 2, #alls, 1 do  
         -- 一行中，每一列的内容,第一位为ID  
         local content = split(alls[i], ",")  
-        ID = tonumber(content[1])  
-        --保存ID，以便遍历取用，原来遍历可以使用in pairs来执行，所以这个不需要了  
-        --table.insert(arrs, i-1, ID)  
-        arrs[ID] = {}  
+        arrs[i-1] = {}  
         -- 以标题作为索引，保存每一列的内容，取值的时候这样取：arrs[1].Title  
         for j = 1, #titles, 1 do  
-            arrs[ID][titles[j]] = content[j]  
+            arrs[i-1][titles[j]] = content[j]  
         end  
     end  
     return arrs  
@@ -80,5 +76,6 @@ local arr = loadCsvFile('./MarketData_Year_2015.csv')
 
 for k1,v1 in ipairs(arr) do
 	for k2,v2 in ipairs(v1) do
+		print(k2,v2)
 	end	
 end
